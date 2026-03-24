1abe92d12e527097ee643d5bedd14e2e7e668e70
kcy <cksdud1202a@naver.com>
2026-03-24 00:56:16 -0400

    1

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,13 +1,13 @@
-######################################
+#####################################
 # ALB (Application Load Balancer)
 # aws_lb.main 리소스는 제거됨.
 # ALB 전체 라이프사이클(생성·리스너·타겟그룹·삭제)은
 # AWS Load Balancer Controller(LBC)가 k8s/aws-lbc/ingress.yaml의
 # Ingress 어노테이션을 통해 단독 관리함.
 # terraform이 ALB를 미리 생성하면 DuplicateLoadBalancerName 충돌 발생.
-######################################
+#####################################
 
-######################################
+#####################################
 # Destroy-time cleanup
 #
 # 실행 순서 (depends_on 역방향):
